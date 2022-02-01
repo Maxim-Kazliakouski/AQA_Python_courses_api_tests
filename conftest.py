61037a908e5c46649b20d39c36f64fb348310b9f
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2022-02-02 00:59:02 +0300

    adding changes

--- conftest.py
+++ conftest.py
@@ -1,6 +1,25 @@
+import atexit
+import os
+import time
+import pytest
+
+
 def pars_for_creation_user(json_response):
     print()
     for i, j in json_response.items():
         print(f'{i}: {j}')
 
 
+# @pytest.fixture(scope='session')
+# def clearing_results_folder():
+#     print('\nClearing results folder...')
+#     time.sleep(5)
+#     os.system("rm -rf results/*")
+#     yield
+#     print('Ending tests...')
+
+
+# def start_allure_report():
+#     print('\nGenerating allure report...')
+#     os.system('allure serve results/')
+#     time.sleep(5)
---
+++ test_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# pytest tests_api > result.txt
+echo -e 'Script for startign api tests and generating report\n'
+if [ -f result.txt ]
+then
+  echo -e 'Deleting previous results...\n'
+  sleep 2
+  echo -e 'The previos results has been deleted!!!\n'
+  sleep 2
+else
+  echo -e 'There is no result.txt, this file will be created...\n'
+  sleep 2
+fi
+pytest -v --alluredir=results/ tests_api | tee result.txt
+echo 'Generating allure report...'
+sleep 3
+allure serve results/
+
